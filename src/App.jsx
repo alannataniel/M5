b88489e2ee99a2885df64bda880f26e9446b953b
Alan Nataniel <113535400+alannataniel@users.noreply.github.com>
2023-03-03 17:03:08 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,138 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+<div className="container">
+
+<div className="row">
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 1</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 2</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 3</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+<div className="row">
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 4</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+  <div className="col-md m-3">
+    
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 5</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 6</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+<div className="row">
+
+  <div className="col-md m-3">
+
+    <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Card title 7</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+  </div>
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 8</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+  <div className="col-md m-3">
+
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Card title 9</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+</div>
+
+    </div>
+  )
+}
+
+export default App
